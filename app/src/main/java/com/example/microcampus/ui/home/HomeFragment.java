f83ec98307b8caecefc1473af6b06f2ab2d9d7df
ColdCancer <svipvm@yeah.net>
2021-05-19 14:06:16 +0800

    add several home schedule function

--- app/src/main/java/com/example/microcampus/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/microcampus/ui/home/HomeFragment.java
@@ -21,33 +21,19 @@ import org.w3c.dom.Text;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private View root;
     private LinearLayout schedule_col, schedule_row;
-    private View schedule_day;
     private LayoutInflater layoutInflater;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        schedule_col = (LinearLayout) root.findViewById(R.id.schedule_col);
-        schedule_row = (LinearLayout) root.findViewById(R.id.schedule_row);
+        init();
+        buildLayout();
 
-        layoutInflater = LayoutInflater.from(root.getContext());
-//        schedule_day = layoutInflater.inflate(R.layout.schedule_day, null, false);
-//        System.out.println(schedule_col);
-//        System.out.println(schedule_day);
-        Resources resources = getResources();
-        String[] days = resources.getStringArray(R.array.days);
-
-        for (int i = 0; i < 7; i++) {
-            View schedule_day = layoutInflater.inflate(R.layout.schedule_day, null, false);
-            TextView content = schedule_day.findViewById(R.id.content);
-            content.setText(days[i]);
-            schedule_col.addView(schedule_day);
-        }
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
@@ -60,4 +46,31 @@ public class HomeFragment extends Fragment {
 //        });
         return root;
     }
+
+    private void buildLayout() {
+        Resources resources = getResources();
+        String[] days = resources.getStringArray(R.array.days);
+
+        for (int i = 0; i < 7; i++) {
+            View schedule_day = layoutInflater.inflate(R.layout.schedule_day, null, false);
+            TextView content = schedule_day.findViewById(R.id.content);
+            content.setText(days[i]);
+            schedule_col.addView(schedule_day);
+        }
+
+        days = resources.getStringArray(R.array.cases);
+        for (int i = 0; i < 6; i++) {
+            View schedule_case = layoutInflater.inflate(R.layout.schedule_case, null, false);
+            TextView content = schedule_case.findViewById(R.id.content);
+            content.setText(days[i]);
+            schedule_row.addView(schedule_case);
+        }
+    }
+
+    private void init() {
+        schedule_col = (LinearLayout) root.findViewById(R.id.schedule_col);
+        schedule_row = (LinearLayout) root.findViewById(R.id.schedule_row);
+        layoutInflater = LayoutInflater.from(root.getContext());
+    }
+
 }
