80eafc9828bfcf7c2e012450e391a0cb2b5b6c07
ColdCancer <svipvm@yeah.net>
2021-05-24 17:22:51 +0800

    add message fragment

--- app/src/main/java/com/example/microcampus/ui/message/MessageFragment.java
+++ app/src/main/java/com/example/microcampus/ui/message/MessageFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,19 +19,42 @@ import com.example.microcampus.R;
 public class MessageFragment extends Fragment {
 
     private MessageViewModel notificationsViewModel;
+    private View root;
+    private Button login, logout;
+    private LinearLayout message_content;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(MessageViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_message, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        root = inflater.inflate(R.layout.fragment_message, container, false);
+
+        init();
+
+        message_content.setVisibility(View.INVISIBLE);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                login.setVisibility(View.INVISIBLE);
+                message_content.setVisibility(View.VISIBLE);
             }
         });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message_content.setVisibility(View.INVISIBLE);
+                login.setVisibility(View.VISIBLE);
+            }
+        });
+
         return root;
+    }
+
+    private void init() {
+        login = (Button) root.findViewById(R.id.login);
+        logout = (Button) root.findViewById(R.id.logout);
+        message_content = (LinearLayout) root.findViewById(R.id.message_content);
     }
 }
